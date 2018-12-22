5162a149b91e885eeb2a6cfaf69b4e52bb3d1ee3
JP <a79073@alunos.uminho.pt>
2018-12-22 16:22:37 +0000

    Modifica pubs.js

--- models/pubs.js
+++ models/pubs.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose')
 
 var Schema = mongoose.Schema
 
-
 var ficheiroSchema = new Schema({
+    titulo: {type: String, required: false},
     tipo: {type: String, required: true},
     caminho: {type: String, required: true}
 })
 
-var eventoSchema = new Schema({
+var eventoSchema = new Schema({  // REVER ESTE CASO *OU MAIS GERAL OU COM SUB EVENTOS*
     titulo: {type: String, required: true},
     data: {type: Date, required: true},
     atividade: {type: String, required: true},
@@ -17,8 +17,28 @@ var eventoSchema = new Schema({
 })
 
 var narracaoSchema = new Schema({
+    titulo: {type: String, required: true},
     texto: {type: String, required: true},
-    autor: {type: String, required: false}
+    autor: {type: String, required: false},
+})
+
+var opiniaoSchema = new Schema({
+    opiniao: {type: String, required: true}
+})
+
+var listaSchema = new Schema({
+    titulo: {type: String, required: true},
+    itens: [{type: String, required: true}]
+})
+
+var elemSchema = new Schema({
+    hashtags: [{type: String, required: true}],
+    
+    opiniao: opiniaoSchema,
+    narracao: narracaoSchema,
+    evento: eventoSchema,
+    ficheiros: [ficheiroSchema],
+    lista: listaSchema
 })
 
 var PubSchema = new Schema({
@@ -26,13 +46,9 @@ var PubSchema = new Schema({
     tipo: {type: String, required: true},
     local: {type: String, required: false},
     publico: {type: Boolean, required: true},
-    hashtags: [{type: String, required: true}],
-    titulo: {type: String, required: false},
-    opiniao: {type: String, required: false},
-    narracao: narracaoSchema,
-    evento: eventoSchema,
-    ficheiros: [ficheiroSchema]
-})
+    tituloPub: {type: String, required: false},
 
+    elems: [{type: elemSchema, required: true}]
+})
 
 module.exports = mongoose.model('Pub', PubSchema, 'pubs')
