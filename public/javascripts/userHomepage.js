25f1f0af2b690132f6ae40775fe5df183a68d3a7
Ricardo Peixoto <ricardopeixoto_97@hotmail.com>
2019-01-28 14:50:20 +0000

    styles perfil

--- public/javascripts/userHomepage.js
+++ public/javascripts/userHomepage.js
@@ -1,5 +1,6 @@
 $(()=>{
 
+    $("#perfilHomepage").load('http://localhost:3000/pubs')
     $("#novaPublicacao").load('http://localhost:3000/pubs/publicar')
     $("#publicacoes").load('http://localhost:3000/pubs')
 
