60547c84745041ba15a62e311976d7b584a28da3
JP <a79073@alunos.uminho.pt>
2019-01-29 00:10:49 +0000

    Criação do importador e adição do ficheiros de backup

--- exportador.sh
+++ exportador.sh
@@ -1,7 +1,8 @@
-mongoexport --db tp-web --collection users --out users.json
-mongoexport --db tp-web --collection pubs --out pubs.json
+mongoexport --db tp-web --collection users --out users.json --jsonArray
+mongoexport --db tp-web --collection pubs --out pubs.json --jsonArray
 mkdir dados
 mv users.json dados
 mv pubs.json dados
 cp -r uploaded dados
-tar -zcvf dados.tar.gz dados
+tar -zcf dados.tar.gz dados
+rm -r dados
